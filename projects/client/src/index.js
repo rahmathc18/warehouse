2ea07d24da9c8eb595e349e9275cb00dce97cda4
Rahmat Totong <rahmattotong18@gmail.com>
2023-07-04 18:20:47 +0800

    first commit

---
+++ projects/client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+
+// redux
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </Provider>
+);
---
+++ projects/server/src/routes/index.js
@@ -0,0 +1,29 @@
+const userRouters = require("./userRouters");
+const adminUserRouters = require("./adminUserRouters");
+const adminWarehouseRouters = require("./adminWarehouseRouters");
+const rajaOngkirRouters = require("./rajaOngkirRouters");
+const categoryRouters = require("./categoryRouters");
+const adminProductRouters = require("./adminProductRouters");
+const productRouters = require("./productRouters");
+const adminStockRouters = require("./adminStockRouters");
+const userProfileRouters = require("./userProfileRouters");
+const adminMutationRouters = require("./adminMutationRouters");
+const userOrderRouters = require("./userOrderRouters");
+const userAddressRouters = require("./userAddressRouters");
+const adminStockReportRouters = require("./adminStockReportRouters");
+
+module.exports = {
+    userRouters,
+    adminUserRouters,
+    adminWarehouseRouters,
+    rajaOngkirRouters,
+    categoryRouters,
+    adminProductRouters,
+    productRouters,
+    adminStockRouters,
+    userProfileRouters,
+    adminMutationRouters,
+    userOrderRouters,
+    userAddressRouters,
+    adminStockReportRouters,
+};
