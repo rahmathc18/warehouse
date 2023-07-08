f73e8b0852ec3ef73d6431ca89da538d248bc2bc
Sundus Ardian <sundusardian@gmail.com>
2023-07-08 14:14:20 +0700

    create fetch order list

--- projects/server/src/controllers/userTransactionController.js
+++ projects/server/src/controllers/userTransactionController.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, Op } = require("sequelize");
 
 //import model
 const db = require("../models");
@@ -6,25 +6,76 @@ const user = db.user;
 const product = db.product;
 const transaction = db.transaction;
 const transaction_item = db.transaction_item;
+const order_status = db.order_status;
+const user_address = db.user_address;
+const product_location = db.product_location;
+const warehouse_location = db.warehouse_location;
 
 module.exports = {
   fetchAll: async (req, res) => {
     try {
-      const { user_id } = req.query;
+      const { page, user_id, status } = req.query;
+
+      const condition = {
+        user_id: user_id,
+        order_status_id: status ? status : { [Op.not]: null },
+      };
 
       const data = await transaction.findAll({
-        where: {
-          user_id: user_id,
-        },
+        limit: 10,
+        offset: page ? +page * 10 : 0,
+        where: condition,
         include: [
           {
             model: transaction_item,
+            include: [
+              {
+                model: product_location,
+                attribute: ["qty", "product"],
+                include: [
+                  {
+                    model: product,
+                  },
+                  {
+                    model: warehouse_location,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            model: order_status,
+            attribute: ["status", "id"],
+          },
+          {
+            model: user_address,
+            attributes: {
+              exclude: ["createdAt", "updatedAt", "user_id"],
+            },
           },
         ],
       });
 
       console.log(data);
       res.json(data);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send("Failed");
+    }
+  },
+  updateStatusTransaction: async (req, res) => {
+    try {
+      const { body, params, files } = req;
+      const { status } = body;
+      // console.log(data);
+
+      if (req.files) {
+      }
+
+      res.json({
+        success: true,
+        message: "Status telah diupdate!",
+      });
     } catch (error) {
       console.log(error);
       res.status(400).send("Failed");
