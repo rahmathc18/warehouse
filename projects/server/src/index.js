e094159b5560c50ff45c0349fb12698eb6218b25
Rahmat Totong <rahmattotong18@gmail.com>
2023-07-10 17:54:01 +0800

    test

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -25,32 +25,32 @@ app.use("/Public", express.static(path.join(__dirname, "Public")));
 // NOTE : Add your routes here
 
 app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
+    res.send(`Hello, this is my API`);
 });
 
 app.get("/api/greetings", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello, Student !",
-  });
+    res.status(200).json({
+        message: "Hello, Student !",
+    });
 });
 
 //routes
 const {
-  userRouters,
-  adminUserRouters,
-  adminWarehouseRouters,
-  rajaOngkirRouters,
-  categoryRouters,
-  adminProductRouters,
-  productRouters,
-  userProfileRouters,
-  adminStockRouters,
-  userOrderRouters,
-  adminMutationRouters,
-  userAddressRouters,
-  adminStockReportRouters,
-  adminTransactionRouters,
-  userTransactionRouters,
+    userRouters,
+    adminUserRouters,
+    adminWarehouseRouters,
+    rajaOngkirRouters,
+    categoryRouters,
+    adminProductRouters,
+    productRouters,
+    userProfileRouters,
+    adminStockRouters,
+    userOrderRouters,
+    adminMutationRouters,
+    userAddressRouters,
+    adminStockReportRouters,
+    adminTransactionRouters,
+    userTransactionRouters,
 } = require("./routes/index");
 
 //users
@@ -80,21 +80,21 @@ app.use("/api", rajaOngkirRouters);
 
 // not found
 app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
-  } else {
-    next();
-  }
+    if (req.path.includes("/api/")) {
+        res.status(404).send("Not found !");
+    } else {
+        next();
+    }
 });
 
 // error
 app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
-  } else {
-    next();
-  }
+    if (req.path.includes("/api/")) {
+        console.error("Error : ", err.stack);
+        res.status(500).send("Error !");
+    } else {
+        next();
+    }
 });
 
 //#endregion
@@ -105,16 +105,16 @@ app.use(express.static(join(__dirname, clientPath)));
 
 // Serve the HTML page
 app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
+    res.sendFile(join(__dirname, clientPath, "index.html"));
 });
 
 //#endregion
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(`ERROR: ${err}`);
-  } else {
-    // db.sequelize.sync({ alter: true });
-    console.log(`APP RUNNING at ${PORT} ✅`);
-  }
+    if (err) {
+        console.log(`ERROR: ${err}`);
+    } else {
+        // db.sequelize.sync({ alter: true });
+        console.log(`APP RUNNING at ${PORT} ✅✅`);
+    }
 });
